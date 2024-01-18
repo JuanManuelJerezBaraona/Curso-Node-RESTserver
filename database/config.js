d24a6f9b624f8546e2f0245d5528d804ede0b421
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-18 09:54:05 -0300

    Refactor database connection and add server.js changes

--- database/config.js
+++ database/config.js
@@ -3,13 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        await mongoose.connect(process.env.MONGODB_CNN, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-
+        await mongoose.connect(process.env.MONGODB_CNN);
         console.log('✅ Base de datos online');
 
     } catch (error) {
