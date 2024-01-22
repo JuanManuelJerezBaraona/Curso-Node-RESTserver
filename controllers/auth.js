78fc5dee63b74a562b02cad4d27b868a23bd6914
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-22 18:43:14 -0300

    Add login functionality with JWT authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,53 @@
 const { response } = require("express");
+const bcryptjs = require('bcryptjs');
 
+const User = require('../models/user');
 
-const login = (req, res = response) => {
+const { generateJWT } = require("../helpers/generate-jwt");
 
-    res.json({
-        msg: 'Login ok'
-    })
+const login = async (req, res = response) => {
 
+    const { email, password } = req.body;
+
+    try {
+
+        // Verificar email
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - email'
+            });
+        }
+
+        // Verificar si el usuario está activo
+        if (!user.status) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - status: false'
+            });
+        }
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generateJWT(user.id);
+
+        res.json({
+            user,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 }
 
 module.exports = {
