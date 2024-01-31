32d38a5a1cde41d9f782cc64ba2a00ed5191f20c
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-31 16:55:03 -0300

    Add searchUsers function to search for users by ID

--- controllers/search.js
+++ controllers/search.js
@@ -1,15 +1,57 @@
 const { response } = require("express");
+const { ObjectId } = require('mongoose').Types;
 
+const { User, Category, Product } = require("../models");
+
+const permittedCollections = [
+    'usuarios',
+    'categoria',
+    'productos',
+    'roles'
+];
+
+const searchUsers = async (term = '', res = response) => {
+
+    const isMongoID = ObjectId.isValid(term);
+
+    if (isMongoID) {
+        const user = await User.findById(term);
+        return res.json({
+            results: (user) ? [user] : []
+        })
+    }
+}
 
 const search = (req, res = response) => {
     
     const { collection, term } = req.params;
 
-    res.json({
-        collection, term
-    })
+    if(!permittedCollections.includes(collection)){
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${permittedCollections}`
+        })
+    }
+
+    switch (collection) {
+        case 'usuarios':
+            searchUsers(term, res);
+        break;
+        case 'categoria':
+        
+        break;
+        case 'productos':
+        
+        break;
+
+        default:
+            res.status(500).json({
+                msg: 'Se me olvidó hacer esta búsqueda'
+            })
+            break;
+    }
 }
 
 module.exports = {
-    search
+    search,
+    searchUsers
 }
